40257bb3d5a4e821a94cb88c627d15f201cf4eab
tedi kpognon <tedi.kpognon@epsi.fr>
2020-10-14 12:02:24 +0200

    final

--- MyApp/Service/RegionService.cs
+++ MyApp/Service/RegionService.cs
@@ -19,5 +19,50 @@ namespace MyApp.Service
             this._departementServices = departementServices;
             this._CommuneService = communeService;
         }
+        public Region ajouterRegion()
+        {
+            Region r = new Region();
+            r.nomR = _DemandeALutilisateur.saisieNom("Saisissez le nom de votre region");
+            r.codeR = _DemandeALutilisateur.saisieEntier("Saisissez le code du region");
+
+            return r;
+        }
+
+        public void calculNbtotalHabs(List<Commune> listcommunes)
+        {
+            int Nbtot = 0;
+            foreach (Commune c in listcommunes)
+            {
+                Nbtot = Nbtot + c.NbHab;
+            }
+            var culture = CultureInfo.GetCultureInfo("en-GB");
+            string nb = string.Format(culture, "{0:n0}", Nbtot);
+            nb = nb.Replace(",", ".");
+            string message = "Nombre total d'habitants: " + nb;
+            Console.WriteLine(message);
+        }
+        public void afficheRegion(List<Departement> listdepartement, List<Commune> listecommune, List<Region> listregion)
+        {
+            Console.WriteLine("Departements:");
+            foreach(Region r in listregion)
+            {
+                string message = "Nom: " + r.nomR + ", code: " + r.codeR;
+                Console.WriteLine(message);
+            }
+            Console.WriteLine("commune:");
+            foreach (Departement d in listdepartement)
+            {
+                string message = "Nom: " + d.nom + ", code: " + d.numD;
+                Console.WriteLine(message);
+            }
+            Console.WriteLine("commune:");
+            foreach (Commune c in listecommune)
+            {
+                string message_p1 = "Nom: " + c.Nom + " Code Postal: " + c.CodePost;
+                string message_p2 = "Nombre d'habitants: " + c.NbHab;
+                Console.WriteLine(message_p1);
+                Console.WriteLine(message_p2);
+            }
+        }
     }
 }
